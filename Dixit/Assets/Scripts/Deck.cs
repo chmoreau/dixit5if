afe7a0b306844df36e805f7652a77249cd29b829
Amaranth YAN <amaranth.yan@gmail.com>
2016-10-03 17:56:55 +0200

    modify Deck/Hand, hand distrubution and zoom in/out on selected card; modify scene ui

--- Dixit/Assets/Scripts/Deck.cs
+++ Dixit/Assets/Scripts/Deck.cs
@@ -13,20 +13,20 @@ public class Deck : MonoBehaviour {
     private float m_DrawCardDuration = 1.0f;
     [Header("Test")]
     [SerializeField]
-    private Transform m_TargetHandAnchorPoint = null;
+    private Hand m_Hand = null;
 
     void Start()
     {
-        Draw(string.Empty);
+        //Draw(string.Empty);
     }
 
-	public void Draw(string cardId)
+	public void Draw(string cardId, int handIndex)
     {
-        IEnumerator drawCoroutine = DrawCardToHand(m_TargetHandAnchorPoint);
+        IEnumerator drawCoroutine = DrawCardToHand(cardId, handIndex);
         StartCoroutine(drawCoroutine);
     }
 
-    IEnumerator DrawCardToHand(Transform targetHandAnchorPoint)
+    IEnumerator DrawCardToHand(string cardId, int handIndex)
     {
         Card card = Instantiate(m_CardPrefab, m_CardSpawnPoint.position, m_CardSpawnPoint.rotation) as Card;        
         float timer = m_DrawCardDuration;
@@ -34,10 +34,10 @@ public class Deck : MonoBehaviour {
         {
             timer -= Time.deltaTime;
             float process = (m_DrawCardDuration - timer) / m_DrawCardDuration;
-            card.transform.position = Vector3.Lerp(m_CardSpawnPoint.position, targetHandAnchorPoint.position, process);
-            card.transform.rotation = Quaternion.Slerp(m_CardSpawnPoint.rotation, targetHandAnchorPoint.rotation, process);
+            card.transform.position = Vector3.Lerp(m_CardSpawnPoint.position, m_Hand.HandAnchors[handIndex].HandAnchorPoint.position, process);
+            card.transform.rotation = Quaternion.Slerp(m_CardSpawnPoint.rotation, m_Hand.HandAnchors[handIndex].HandAnchorPoint.rotation, process);
             yield return new WaitForEndOfFrame();
         }
-
+        m_Hand.HandAnchors[handIndex].HandCard = card;
     }
 }
--- Dixit/Assets/Scripts/Hand.cs
+++ Dixit/Assets/Scripts/Hand.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class Hand : MonoBehaviour {
     [Serializable]
-    private struct HandAnchor
+    public struct HandAnchor
     {
         [SerializeField]
         private Card m_HandCard;
@@ -17,4 +17,74 @@ public class Hand : MonoBehaviour {
 
     [SerializeField]
     private HandAnchor[] m_HandAnchors = new HandAnchor[6];
+    public HandAnchor[] HandAnchors { get { return m_HandAnchors; } }
+    [SerializeField]
+    private Transform m_ZoomTargetPoint = null;
+    [SerializeField]
+    private float m_ZoomDuration = 0.5f;
+
+    [Header("Test")]
+    [SerializeField]
+    private Deck m_Deck = null;
+
+    private int m_SelectedCardIndex = -1;
+
+    void Start()
+    {
+        StartCoroutine("DrawInitHand");
+    }
+
+    IEnumerator DrawInitHand()
+    {
+        for (int i = 0; i < m_HandAnchors.Length; i++)
+        {
+            m_Deck.Draw(string.Empty, i);
+            yield return new WaitForSeconds(0.8f);
+        }
+    }
+
+    public void FocusOnCard(int cardIndex)
+    {
+        if (m_SelectedCardIndex == -1)
+        {
+            m_SelectedCardIndex = cardIndex;
+            StartCoroutine("ZoomIn");
+        }
+    }
+
+    public void RestoreFocus()
+    {
+        StartCoroutine("ZoomOut");        
+    }
+
+    //bool isZoomingIn = false;
+    IEnumerator ZoomIn()
+    {
+        //isZoomingIn = true;
+        float timer = m_ZoomDuration;
+        while (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            float process = (m_ZoomDuration - timer) / m_ZoomDuration;
+            m_HandAnchors[m_SelectedCardIndex].HandCard.transform.position = Vector3.Lerp(m_HandAnchors[m_SelectedCardIndex].HandAnchorPoint.position, m_ZoomTargetPoint.position, process);
+            //card.transform.rotation = Quaternion.Slerp(m_CardSpawnPoint.rotation, m_ZoomTargetPoint.rotation, process);
+            yield return new WaitForEndOfFrame();
+        }
+        //isZoomingIn = false;
+    }
+
+    //bool isZoomingO
+    IEnumerator ZoomOut()
+    {
+        float timer = m_ZoomDuration;
+        while (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            float process = (m_ZoomDuration - timer) / m_ZoomDuration;
+            m_HandAnchors[m_SelectedCardIndex].HandCard.transform.position = Vector3.Lerp(m_ZoomTargetPoint.position, m_HandAnchors[m_SelectedCardIndex].HandAnchorPoint.position, process);
+            //card.transform.rotation = Quaternion.Slerp(m_CardSpawnPoint.rotation, m_ZoomTargetPoint.rotation, process);
+            yield return new WaitForEndOfFrame();
+        }
+        m_SelectedCardIndex = -1;
+    }
 }
