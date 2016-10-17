a5bc4b007c65c6cd9d2d38dd1c1128368bb00dd4
Charles Moreau <charles.moreau08@gmail.com>
2016-10-17 15:04:51 +0200

    fix message type export

--- backend/messageType.js
+++ backend/messageType.js
@@ -1,17 +1,19 @@
-export const JOIN_MATCHMAKING = 'JOIN_MATCHMAKING';
-export const QUEUE_SIZE = 'QUEUE_SIZE';
-export const GAME_CREATED = 'GAME_CREATED';
-export const PLAYER_READY = 'PLAYER_READY';
-export const CARDS = 'CARDS';
-export const NARRATOR = 'NARRATOR';
-export const THEME = 'THEME';
-export const PLAY_CARD = 'PLAY_CARD';
-export const CARD_PLAYED = 'CARD_PLAYED';
-export const REVEAL_CARD = 'REVEAL_CARD';
-export const PICK_CARD = 'PICK_CARD';
-export const CARD_PICKED = 'CARD_PICKED';
-export const TRICK = 'TRICK';
-export const NEW_TURN = 'NEW_TURN';
-export const GAME_OVER = 'GAME_OVER';
-export const CONTEXT = 'CONTEXT';
+module.exports = Object.freeze({
+    JOIN_MATCHMAKING : 'JOIN_MATCHMAKING',
+    QUEUE_SIZE : 'QUEUE_SIZE',
+    GAME_CREATED : 'GAME_CREATED',
+    PLAYER_READY : 'PLAYER_READY',
+    CARDS : 'CARDS',
+    NARRATOR : 'NARRATOR',
+    THEME : 'THEME',
+    PLAY_CARD : 'PLAY_CARD',
+    CARD_PLAYED : 'CARD_PLAYED',
+    REVEAL_CARD : 'REVEAL_CARD',
+    PICK_CARD : 'PICK_CARD',
+    CARD_PICKED : 'CARD_PICKED',
+    TRICK : 'TRICK',
+    NEW_TURN : 'NEW_TURN',
+    GAME_OVER : 'GAME_OVER',
+    CONTEXT : 'CONTEXT'
+});
 
